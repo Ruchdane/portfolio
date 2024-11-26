59ee3772c82be29ae0f61c6a892c285b1468d5bd
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2024-11-26 13:05:25 +0100

    feat: setup navbar, sidebar and burger trigger

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,24 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_LOGO = "/logo.svg";
+export const SITE_TITLE = "Ruchdane";
+export const SITE_DESCRIPTION = "Hi I'm AMADOU Ruchdane a software engineer";
+export const NAVIGATION_LINKS = [
+  {
+    link: "/",
+    title: "Acceuil",
+  },
+  {
+    link: "/projets",
+    title: "Projets",
+  },
+  {
+    link: "/blog",
+    title: "Expériences",
+  },
+  {
+    link: "/about",
+    title: "Blogs",
+  },
+];
