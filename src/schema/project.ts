a9bff5309cc4d9b5e40515f003a3687d3143d727
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2024-11-30 17:35:41 +0100

    feat: add project page

--- src/schema/project.ts
+++ src/schema/project.ts
@@ -2,7 +2,7 @@ import { z } from "astro:content";
 import { ImageSchema } from "@/schema/image";
 import { TechnologieEnum, PlatformeEnum } from "@/schema/enum";
 export const ProjectSchema = z.object({
-  logo: z.string().optional(),
+  logo: z.string(),
   title: z.string(),
   description: z.string(),
   thumbs: z.array(ImageSchema).min(1),
