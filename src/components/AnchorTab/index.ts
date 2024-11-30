c839e8bc0e23f625d6cec76490b129e1a96c3730
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2024-11-30 22:04:51 +0100

    refactor: remove all relative import looking for parent
    - add barel file to all folder

---
+++ src/components/AnchorTab/index.ts
@@ -0,0 +1,3 @@
+export {default as AnchorTabs} from "./AnchorTabs.astro"
+export {default as TabContent} from "./TabContent.astro"
+export {default as TabTrigger} from "./TabTrigger.astro"
---
+++ src/components/IconLinks/index.ts
@@ -0,0 +1,4 @@
+export { default as GithubIconLink } from "./GithubIconLink.astro"
+export { default as IconLink } from "./IconLink.astro"
+export { default as LinkedinIconLink } from "./LinkedinIconLink.astro"
+export { default as MailIconLink } from "./MailIconLink.astro"
