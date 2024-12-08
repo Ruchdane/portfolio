dedaa231bd727267ade5229864583daa06cf66bf
ruchdane <ruchdaneabiodun@gmail.com>
2024-12-08 15:24:03 +0100

    feat: add PeopleProfil component
    - feat: add SocialLinks component
    - refactor: move social links from index page into component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,16 @@
-export * from "./AnchorTab"
-export * from "./IconLinks"
-export * from "./Carousel"
+export * from "./AnchorTab";
+export * from "./IconLinks";
+export * from "./Carousel";
 
-export {default as BaseHead} from "./BaseHead.astro"
-export {default as Footer} from "./Footer.astro"
-export {default as FormattedDate} from "./FormattedDate.astro"
-export {default as Header} from "./Header.astro"
-export {default as HeaderLink} from "./HeaderLink.astro"
-export {default as HistoryElement} from "./HistoryElement.astro"
-export {default as MorphingBurger} from "./MorphingBurger.astro"
-export {default as ProjectCard} from "./ProjectCard.astro"
-export {default as Sidebar} from "./Sidebar.astro"
-export {default as TechnologieBadge} from "./TechnologieBadge.astro"
+export { default as BaseHead } from "./BaseHead.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as FormattedDate } from "./FormattedDate.astro";
+export { default as Header } from "./Header.astro";
+export { default as HeaderLink } from "./HeaderLink.astro";
+export { default as HistoryElement } from "./HistoryElement.astro";
+export { default as MorphingBurger } from "./MorphingBurger.astro";
+export { default as ProjectCard } from "./ProjectCard.astro";
+export { default as Sidebar } from "./Sidebar.astro";
+export { default as TechnologieBadge } from "./TechnologieBadge.astro";
+export { default as PeopleProfile } from "./PeopleProfile.astro";
+export { default as SocialLinks } from "./SocialLinks.astro";
