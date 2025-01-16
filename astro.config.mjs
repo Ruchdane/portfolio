b2069203df5877d482be04c4c7ddaeb1af5d1e95
ruchdane <ruchdaneabiodun@gmail.com>
2025-01-16 10:55:23 +0100

    feat: add compression to reduce static assets size
    - fix: change meta image to banner picture
    - chore(deps): add astro-compres

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ruchdane.netlify.app",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), compress()],
 });
