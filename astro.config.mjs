bb1cf862733ac92d63fe4cff7434f6e3d8987079
ruchdane <ruchdaneabiodun@gmail.com>
2025-04-19 21:06:53 +0100

    build: add purgecss to reduce the size of deviconcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import compress from "astro-compress";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "never",
+  },
   site: "https://ruchdane.me",
-  integrations: [mdx(), sitemap(), compress()],
+  integrations: [mdx(), sitemap(), compress(), purgecss()],
   experimental: {
     svg: true,
   },
