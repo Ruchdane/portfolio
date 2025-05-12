67dc8fe12bdbdf19aad59a2d70ed1021f7024b07
ruchdane <ruchdaneabiodun@gmail.com>
2025-05-12 10:18:34 +0100

    feat: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import purgecss from "astro-purgecss";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   build: {
     inlineStylesheets: "never",
   },
   site: "https://ruchdane.me",
-  integrations: [mdx(), sitemap(), compress(), purgecss()],
+  integrations: [mdx(), sitemap(), partytown(), compress(), purgecss()],
   image: {
     experimentalLayout: "responsive",
   },
