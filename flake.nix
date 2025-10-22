eb6755332afd1b41d00e73a43b8692d6f145b636
AMADOU Ruchdane <ruchdaneabiodun@gmail.com>
2025-10-22 21:53:35 +0200

    feat: add flake configuration for Node.js development environment

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Development environment for a Node.js project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        unstablePkgs = import unstable { inherit system; };
+      in
+      {
+        # to use other shells, run:
+        # nix develop . --command fish
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.nodePackages.pnpm
+          ];
+
+          shellHook = ''
+            echo "node: $(node -v)"
+            echo "pnpm: $(pnpm -v)"
+            echo "To install dependencies, run: pnpm install"
+          '';
+        };
+
+        packages.default = pkgs.writeShellScriptBin "setup-project" ''
+          pnpm install --frozen-lockfile
+        '';
+      });
+}
