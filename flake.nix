0f5ecde8b52a35d149f0ba89179a975dfc8f8f6b
ruchdane <ruchdaneabiodun@gmail.com>
2026-07-18 10:05:13 +0200

    chore: update environnement dependencies

--- flake.nix
+++ flake.nix
@@ -2,24 +2,23 @@
   description = "Development environment for a Node.js project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    # unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, unstable, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        unstablePkgs = import unstable { inherit system; };
       in
       {
         # to use other shells, run:
         # nix develop . --command fish
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.nodejs_20
-            pkgs.nodePackages.pnpm
+            pkgs.nodejs_26
+            pkgs.pnpm_11
             pkgs.ripgrep
             pkgs.htmlq
             pkgs.woff2
